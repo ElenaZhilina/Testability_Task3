8b2d350fe69c4ad8f5d47f18160429699e4ddaf8
Elena <elenazhilina@yandex.ru>
2024-02-24 19:41:35 +0600

    Code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+                CreditPaymentService service = new CreditPaymentService ();
+        double payment = service.calculate(9.99, 1000000, 24);
+        System.out.println(payment);
     }
 }
