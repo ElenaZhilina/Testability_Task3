2276422116254d26ce572a8fd46cc2ae93d0939e
Elena <elenazhilina@yandex.ru>
2024-02-25 09:11:58 +0600

    Formula changed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(double percent, double sumRub, double termMonth) {
-        double v = 1 + percent / 12;
-        return (sumRub * (((percent / 12) * Math.pow(v, termMonth)) / (Math.pow(v, termMonth) - 1)));
+        double v = 1 + percent / 12 / 100;
+        return (sumRub * (((percent / 12 / 100) * Math.pow(v, termMonth)) / (Math.pow(v, termMonth) - 1)));
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
                 CreditPaymentService service = new CreditPaymentService ();
-        double payment = service.calculate(9.99, 1000000, 24);
+        int payment = (int) service.calculate(9.99, 1000000, 12);
+        System.out.println(payment);
+
+        payment = (int) service.calculate(9.99, 1000000, 24);
+        System.out.println(payment);
+
+        payment = (int) service.calculate(9.99, 1000000, 36);
         System.out.println(payment);
     }
 }
